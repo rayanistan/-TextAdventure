fd19003c9b6c0d4b77dc67787c6520675884d9ef
Rayan Ansari <rayansri@gmail.com>
2016-12-21 15:21:53 -0800

    FIXEL ALL SPRITES, added blink too. Did not make the atlas Just put them in a folder in ur special folder for sprites.

--- core/src/com/rayanistan/game/NotTextAdventure.java
+++ core/src/com/rayanistan/game/NotTextAdventure.java
@@ -1,5 +1,4 @@
 package com.rayanistan.game;
-
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
--- desktop/src/com/rayanistan/game/desktop/DesktopLauncher.java
+++ desktop/src/com/rayanistan/game/desktop/DesktopLauncher.java
@@ -7,7 +7,6 @@ import com.rayanistan.game.NotTextAdventure;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.useGL30 = true;
 		new LwjglApplication(new NotTextAdventure(), config);
 	}
 }
