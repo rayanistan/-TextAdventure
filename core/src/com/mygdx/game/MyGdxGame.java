919ddf92f0f17b3f708adece613ad73e4dcdef1e
Rayan Ansari <rayansri@gmail.com>
2016-12-20 17:57:39 -0800

    Added new sprite called SWORD.png, I added a todo so Kosi if you could help me with that. That would be cool. ty

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,70 +1,82 @@
 package com.mygdx.game;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
 
+import javax.xml.soap.Text;
 import java.awt.*;
 
 public class MyGdxGame extends ApplicationAdapter {
+
 	private SpriteBatch batch;
-
-
 	private Animation animation2;
 	private TextureAtlas textureAtlas2;
+
 
 	private TextureAtlas textureAtlas;
 	private Animation animation;
 	private float elapsedTime = 0;
-    private BitmapFont font;
+	private BitmapFont font;
+
 	private Sprite sprite;
 	private Texture texture;
 
 
+	private Texture weptexture;
 
 	private OrthographicCamera camera;
 
 
+	private enum Direction {
+		RIGHT,
+		LEFT,
+		STILL
+	}
+
+
+	private Direction direction;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-
-		//move right animation
 		textureAtlas = new TextureAtlas(Gdx.files.internal("walk-packed/pack.atlas"));
-		animation = new Animation(1/11f, textureAtlas.getRegions());
+		animation = new Animation(1 / 11f, textureAtlas.getRegions());
 
-		//move left animation
 		textureAtlas2 = new TextureAtlas(Gdx.files.internal("walk2-packed/pack.atlas"));
-		animation2 = new Animation(1/11f, textureAtlas2.getRegions());
+		animation2 = new Animation(1 / 11f, textureAtlas2.getRegions());
+
 
 		//misc
-        font = new BitmapFont();
+		font = new BitmapFont();
 		font.setColor(Color.WHITE);
 
-		//when not moving
+
 		texture = new Texture(Gdx.files.internal("stand.png"));
 		sprite = new Sprite(texture);
 
+		weptexture = new Texture(Gdx.files.internal("SWORD.png"));
 
+
+		direction = Direction.STILL;
 
 
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+		camera.setToOrtho(false, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
 	}
-    int x = 0;
+
+
+	int x = 0;
 
 	@Override
-	public void render () {
+	public void render() {
 
 
 		Gdx.gl.glClearColor(255, 255, 255, 255);
@@ -72,8 +84,9 @@ public class MyGdxGame extends ApplicationAdapter {
 
 
 		Vector3 position = camera.position;
-		position.x = MathUtils.lerp(position.x, sprite.getX()+sprite.getWidth()/2, 0.1f);
-		position.y = MathUtils.lerp(position.y, sprite.getY()+sprite.getHeight()/2, 0.1f);
+		position.x = MathUtils.lerp(position.x, sprite.getX() + sprite.getWidth() / 2, 0.1f);
+		position.y = MathUtils.lerp(position.y, sprite.getY() + sprite.getHeight() / 2, 0.1f);
+
 		camera.position.set(position);
 		camera.update();
 
@@ -82,44 +95,74 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.begin();
 
 
-		if(Gdx.input.isKeyPressed(Input.Keys.A) && Gdx.input.isKeyPressed(Input.Keys.D) ){
-			sprite.draw(batch);
+
+		if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+			x += 2;
+			TextureRegion frame = animation2.getKeyFrame(elapsedTime, true);
+			sprite.setBounds(frame.getRegionX(), frame.getRegionY(), frame.getRegionWidth(), frame.getRegionHeight());
+			sprite.setRegion(frame);
+			direction = Direction.RIGHT;
+		} else if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+			x -= 2;
+			TextureRegion frame = animation2.getKeyFrame(elapsedTime, true);
+			sprite.setBounds(frame.getRegionX(), frame.getRegionY(), frame.getRegionWidth(), frame.getRegionHeight());
+			sprite.setRegion(frame);
+			direction = Direction.LEFT;
+		} else {
+			sprite.setBounds(0, 0, texture.getWidth(), texture.getHeight());
+			sprite.setRegion(texture);
 		}
-		else if(Gdx.input.isKeyPressed(Input.Keys.D)){
-			x+=2;
-			batch.draw(animation.getKeyFrame(elapsedTime, true), x ,170 );
+		if(Gdx.input.isButtonPressed(Input.Buttons.RIGHT)){
+			sprite.setBounds(0, 0, weptexture.getWidth(), weptexture.getHeight());
+			sprite.setTexture(weptexture);
 
 		}
-		else if(Gdx.input.isKeyPressed(Input.Keys.A)){
-         x-=2;
-         batch.draw(animation2.getKeyFrame(elapsedTime, true), x, 170);
+		//TODO: ^THE IF STATEMENT SHOULD CHANGE THE TEXTURE PERMANENTElY, UNTIL USER RE-CLICKS THE RIGHT BUTTON
 
+		switch (direction) {
+			default:
+			case STILL:
+				sprite.setFlip(false, false);
+				break;
+			case LEFT:
+				sprite.setFlip(false, false);
+				break;
+			case RIGHT:
+				sprite.setFlip(true, false);
+				break;
 		}
-		else if(!Gdx.input.isKeyPressed(Input.Keys.D)){
-			sprite.draw(batch);
-		}
+
+
 
 		sprite.setX(x);
 		sprite.setY(170);
 
 
+		sprite.draw(batch);
+		elapsedTime += Gdx.graphics.getDeltaTime();
 
-		elapsedTime+= Gdx.graphics.getDeltaTime();
 
 		batch.end();
-	}
-    @Override
-	public void resize(int width, int height){
 
-		camera.setToOrtho(false, width/2, height/2);
 	}
 	@Override
-	public void dispose () {
+	public void resize ( int width, int height){
+
+		camera.setToOrtho(false, width / 2, height / 2);
+	}
+
+
+	@Override
+	public void dispose(){
 		batch.dispose();
 		textureAtlas.dispose();
 		font.dispose();
 		texture.dispose();
 		textureAtlas2.dispose();
+		weptexture.dispose();
 
 	}
+
+
 }
+
