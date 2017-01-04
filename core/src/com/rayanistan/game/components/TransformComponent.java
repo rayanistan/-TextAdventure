5ab2e81038d4f79808d5afe66ebcf1868579508d
Rayan Ansari <rayansri@gmail.com>
2017-01-04 13:44:39 -0800

    no message

--- core/src/com/rayanistan/game/components/TransformComponent.java
+++ core/src/com/rayanistan/game/components/TransformComponent.java
@@ -1,6 +1,7 @@
 package com.rayanistan.game.components;
 
 import com.badlogic.ashley.core.Component;
+
 import com.badlogic.gdx.math.Vector2;
 
 public class TransformComponent implements Component {
